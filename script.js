858e99f391495c6992779283dd681623fc4c6d0d
Natan Tibebu <natantibebu111@gmail.com>
2025-08-27 14:14:20 +0300

    Frist commit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function change(){
-    const input = document.getElementsByTagName('input').value.trim()
-    const p = document.getElementsByTagName('p')
-    if(input.toLowerCase() === "bold"){
-      p.style.fontWeight = 'bold';
-      
-    }
-    else{
-        p.innerText = 'Nothing changed';
-    }
+function changeStyle(){
+  const input = document.getElementById('styleInput');
+  const p = document.getElementById('p');
+  const editedInput = input.value.trim()
+
+  p.style.fontWeight = '';
+  p.style.fontStyle = '';
+  p.style.textDecoration = '';
+  input.value ="";
+  
+
+ if(editedInput.toLowerCase() === 'bold'){
+  p.style.fontWeight = 'bold';
+ }
+
+  if(editedInput.toLowerCase() === 'italic'){
+  p.style.fontStyle = 'italic';
+ }
+
+ if(editedInput.toLowerCase() === 'underline'){
+  p.style.textDecoration = 'underline';
+ }
+
 }
